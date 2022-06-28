8a5f838ebc04f68566111e075936520fc50d42f6
Pulse <shelvanek@mail.ru>
2022-06-28 21:00:29 +0300

    dz2

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,11 +1,19 @@
-﻿Console.WriteLine("Введите два числа: ");
+﻿Console.WriteLine("Введите три числа: ");
 int f = Convert.ToInt32(Console.ReadLine());
 int a = Convert.ToInt32(Console.ReadLine());
-if (f>a)
+int b = Convert.ToInt32(Console.ReadLine());
+if (f>a && f>b)
 {
     Console.WriteLine("max = " + f);
 }
 else
 {
-    Console.WriteLine("max = " + a);
+    if (a>b)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else
+    {
+        Console.WriteLine("max = " + b);
+    }
 }
